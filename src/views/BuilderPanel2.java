e681893c533f7f9f0dc1074dc44504df958f0ae6
OAkyildiz <oakyildiz@wpi.edu>
2015-04-15 13:26:26 -0400

    ..

---
+++ src/views/BuilderPanel2.java
@@ -0,0 +1,66 @@
+package views;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.SwingConstants;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+
+import controllers.ExitController;
+
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JInternalFrame;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+
+import java.awt.ComponentOrientation;
+
+import javax.swing.JScrollBar;
+import javax.swing.JSpinner;
+import javax.swing.JSlider;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+/*
+ * 
+ */
+
+public class BuilderPanel2 extends JPanel implements IApplication{
+	public BuilderPanel2() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblLevelBuilder = new JLabel("Level Builder");
+		lblLevelBuilder.setFont(new Font("Tahoma", Font.BOLD, 14));
+		GridBagConstraints gbc_lblLevelBuilder = new GridBagConstraints();
+		gbc_lblLevelBuilder.insets = new Insets(0, 0, 0, 5);
+		gbc_lblLevelBuilder.gridx = 6;
+		gbc_lblLevelBuilder.gridy = 0;
+		add(lblLevelBuilder, gbc_lblLevelBuilder);
+		
+		JButton btnMenu = new JButton("Menu");
+		GridBagConstraints gbc_btnMenu = new GridBagConstraints();
+		gbc_btnMenu.gridx = 13;
+		gbc_btnMenu.gridy = 0;
+		add(btnMenu, gbc_btnMenu);
+	}
+
+	
+}
