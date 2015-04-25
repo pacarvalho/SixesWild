76eef88fea28a95705c9b8081b18f323eab48df4
Paulo <pacarvalho@wpi.edu>
2015-04-25 19:24:16 -0400

    Added SixTile. A type of tyle that cannot be clicked but falls.

---
+++ src/model/SixTile.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Six type tile.
+ * 
+ * This type of tile is a six.
+ * It cannot be clicked but it falls.
+ * 
+ * @author Paulo, Katie
+ *
+ */
+public class SixTile implements ITile{
+
+}
