239f0a2613a168493227013245c53bff9c5599e3
Paulo <pacarvalho@wpi.edu>
2015-05-02 17:21:43 -0400

    Improved redo and undo. Time limit text field workd properly.

---
+++ src/builder/controllers/ResetBoardController.java
@@ -0,0 +1,47 @@
+package builder.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.Board;
+import model.SixesWild;
+import builder.model.BuilderSixesWild;
+import builder.views.BuilderPanel;
+
+public class ResetBoardController implements ActionListener{
+	
+	/** Create Boundary Object */
+	BuilderPanel builderPanel;
+	
+	/** Create Game Model Object*/
+	BuilderSixesWild builder;
+	
+	/** Create Game Model Object*/
+	SixesWild model;
+
+	public ResetBoardController(BuilderPanel builderPanel, BuilderSixesWild builder, SixesWild model){
+		this.builderPanel = builderPanel;
+		this.builder = builder;
+		this.model = model;
+	}
+	
+	/**
+	 * Reset Builder Level Listener
+	 * 
+	 * When this button is click the board resets to its default state
+	 * 
+	 * @param e
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.builder.createMemento(this.model.getBoard());
+		
+		Board defaultBoard = new Board();
+		defaultBoard.createDefaultBoard();
+		
+		this.model.initialize(defaultBoard);
+		
+		this.builderPanel.updateParameters();
+		
+	}
+}
---
+++ src/builder/controllers/timeLimitController.java
@@ -0,0 +1,69 @@
+package builder.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import model.SixesWild;
+import builder.model.BuilderSixesWild;
+import builder.views.BuilderPanel;
+
+/**
+ * Controller for the time limit setting
+ * 
+ * @author Katie, Paulo
+ *
+ */
+public class timeLimitController implements ActionListener{
+	/** Create Boundary Object */
+	BuilderPanel builderPanel;
+	
+	/** Create Game Model Object*/
+	BuilderSixesWild builder;
+	
+	/** Create Game Model Object*/
+	SixesWild model;
+	
+	/** Text Field */
+	JTextField text;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param builderPanel
+	 * @param builder
+	 * @param model
+	 * @param timeLimitText 
+	 */
+	public timeLimitController(BuilderPanel builderPanel, BuilderSixesWild builder, SixesWild model, JTextField text){
+		this.builderPanel = builderPanel;
+		this.builder = builder;
+		this.model = model;
+		this.text = text;
+	}
+
+	/**
+	 * Listener for Text Field Enter Click
+	 * 
+	 * When enter is pressed on the text field, update the model and add to undo stack
+	 * 
+	 * @param e
+	 */
+	public void actionPerformed(ActionEvent e) {
+		
+		this.builder.createMemento(this.model.getBoard()); // Save memento
+		
+		int limit = 0;
+		
+		// If the user enters a non-integer value, assume 0.
+		try{ 
+			limit = Integer.parseInt(this.text.getText());
+		} catch (NumberFormatException z) {
+			System.out.println(z.getStackTrace());
+		}
+		
+		this.model.getBoard().setTimeLimit(limit);
+	}
+
+	
+	
+}
