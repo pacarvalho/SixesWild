8f194934fc402c8100bece2bb9a9eeddd5ed3d1a
Paulo <pacarvalho@wpi.edu>
2015-05-02 17:33:30 -0400

    Missing file that was not added due to some weird git ignore issue

---
+++ src/builder/views/BuilderTileSelectorPanel.java
@@ -0,0 +1,112 @@
+package builder.views;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import builder.model.BuilderSixesWild;
+import model.Tile;
+import views.TileView;
+
+/**
+ * Panel from which user selects which tile he intends to add to board
+ * 
+ * 
+ * @author Paulo, Katie
+ *
+ */
+public class BuilderTileSelectorPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5494638038395128897L;
+	
+	/** Parent Frame */
+	JFrame frame;
+	
+	/** TileView Array */
+	TileView tileViews[];
+	
+	/** Array of tiles */
+	Tile tiles[];
+	
+	/** Current mouse adapter */
+	MouseAdapter activeAdapter;
+	
+	/** Overall Model */
+	BuilderSixesWild builder;
+	
+	/**
+	 * Constructor
+	 */
+	BuilderTileSelectorPanel(JFrame frame, BuilderSixesWild builder){
+		
+		this.frame = frame;
+		
+		this.builder = builder;
+		
+		// Create tile views
+		this.tileViews = new TileView[8];
+		for (int i=0; i<8; i++){
+			this.tileViews[i] = new TileView();
+		}
+
+		// Create Normal Tiles and populate views
+		this.tiles = new Tile[8];
+		for (int i=1; i<7; i++){
+			this.tiles[i] = new Tile(i, i, 0);
+			this.tileViews[i].setTile(this.tiles[i]);
+		}
+		
+		// Wall Tile
+		this.tiles[0] = new Tile(-1,0,0);
+		this.tiles[0].setSelectedFlag(true); // Wall tile starts selected
+		this.builder.setTile(this.tiles[0]);
+		this.tileViews[0].setTile(this.tiles[0]);
+		
+		// Container Tile
+		this.tiles[7] = new Tile(0,7,0);
+		this.tileViews[7].setTile(this.tiles[7]);
+		
+		// Place tileViews in appropriate locations
+		this.setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		for (int i=0; i<8; i++){
+			c.gridx = i;
+			c.gridy = 0;
+			
+			this.add(this.tileViews[i], c);
+		}
+		
+	}
+	
+	/**
+	 * Return the tileViews
+	 */
+	public TileView[] getTileViews(){
+		return this.tileViews;
+	}
+	
+	/** 
+	 * Properly register new listener (and unregister old one if present). 
+	 */
+	public void setActiveAdapter(MouseAdapter ma) {
+		this.removeMouseListener(activeAdapter);
+		this.removeMouseMotionListener(activeAdapter);
+		this.activeAdapter = ma;
+		if (ma != null) { 
+			this.addMouseListener(ma);
+			this.addMouseMotionListener(activeAdapter);
+		}
+	}
+	
+	
+	
+	
+}
